fea5d92c3d0513518fb004766d36f1cc0e81101f
DexterPOSH <dexter34925@yahoo.in>
2016-06-29 14:46:09 +0530

    Fix #5 add Remotely.json

--- Remotely/Remotely.psm1
+++ Remotely/Remotely.psm1
@@ -1,13 +1,5 @@
-﻿# module variables
-$Remotely = @{
-    remotelyNodePath = 'C:\temp\Remotely';
-    modulesRequired = @(
-        @{ModuleName="Pester";ModuleVersion="3.3.14"},
-        @{ModuleName='PoshSpec';ModuleVersion='2.1.6'}
-    )
-    NodeMap = @()
-    sessionHashTable = @{}
-}
+﻿# read the Remotely.json file
+$jsonObject = ConvertFrom-Json -InputObject $(Get-Content -Path "$PSScriptRoot\Remotely.Json" -Raw -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue
 #Get public and private function definition files.
     $public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
     $private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
@@ -29,5 +21,12 @@ $Remotely = @{
     # Read in or create an initial config file and variable
     # Export Public functions ($Public.BaseName) for WIP modules
     # Set variables visible to the module and its functions only
+# read the remotely.json file 
+
+$Remotely = ConvertPSObjectToHashTable -InputObject $jsonObject
+
+# module variables
+$Remotely.Add('NodeMap', @())
+$Remotely.Add('sessionHashTable', @{})
 
 Export-ModuleMember -Function $Public.Basename -Variable Remotely
