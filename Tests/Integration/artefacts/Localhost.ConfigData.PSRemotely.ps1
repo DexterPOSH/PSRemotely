e4fe37bf445e9647b1e2856b5ab15344ef0a02b7
DexterPOSH <dexter34925@yahoo.in>
2016-11-22 14:07:44 +0530

    Add ConfigData Integration Tests

---
+++ Tests/Integration/artefacts/Localhost.ConfigData.PSRemotely.ps1
@@ -0,0 +1,49 @@
+<#
+.Synopsis
+   Example demonstrating use of PS Remotely with Configuration data
+.DESCRIPTION
+   This is a straight forward example showing how to use PS Remotely DSL.
+   To Use:
+     1. Copy to \Tests\Integration\ folder and rename <Name>.PSRemotely.tests.ps1 (e.g. localhost.basic.PSRemoltey.tests.ps1)
+     2. Customize TODO sections.
+     3. Create test PS Remotely test file <Name>.config.ps1 (e.g. localhost.basic.Tests.ps1).
+#>
+
+# Configuration Data
+$ConfigData = @{
+	AllNodes = @(
+		@{
+			NodeName='*';
+			DomainFQDN='dexter.lab';
+		},
+		@{
+			NodeName="$env:ComputerName";
+			ServiceName = 'bits';
+			Type='Compute';
+
+		},
+		@{
+			NodeName='localhost';
+			ServiceName = 'winrm';
+			Type='Storage';
+		}
+	)
+}
+
+# Remotely tests
+Remotely -ConfigurationData $ConfigData {
+	Node $AllNodes.Where({$PSItem.Type -eq 'Compute'}).NodeName {
+		Describe 'Bits Service test' {
+			
+			$Service = Get-Service -Name $node.ServiceName # See the use of $node variable here
+			
+			It "Should have a service named bits" {
+				$Service | Should Not BeNullOrEmpty
+			}
+			
+			it 'Should be running' {
+				$Service.Status | Should be 'Running'
+			}
+		}		
+	}
+}
