b27e842cc824c6cdbf04ff8391c85a658d9c15a5
DexterPOSH <dexter34925@yahoo.in>
2016-11-23 21:29:04 +0530

    Add Integration helpers

--- Tests/TestHelpers/IntegrationEnvironment.psm1
+++ Tests/TestHelpers/IntegrationEnvironment.psm1
@@ -9,12 +9,9 @@
 #>
 
 Function Clear-RemotelyNodePath {
-    param()
+    param($Remotely)
 
-    $RemotelyJSONFile = "$Env:BHPSModulePath\Remotely.json"
-    $RemotelyConfig = ConvertFrom-Json -InputObject (Get-Content $RemotelyJSONFile)
-
-    Remove-Item -Path $RemotelyConfig.RemotelyNodePath -Recurse -Force
+    Remove-Item -Path $Global:Remotely.RemotelyNodePath -Recurse -Force
 }
 
 # Credits : Picked these helpers from the DSC Resources repo 
@@ -125,14 +122,47 @@ function Add-LocalUserToLocalAdminGroup
         $ComputerName = $env:COMPUTERNAME
     )
     Set-StrictMode -Version Latest
-    $AdminGroup = [ADSI]"WinNT://$ComputerName/Administrators,group"
-    $LocalUser = [ADSI]"WinNT://$ComputerName/$userName,user"
-    if ($LocalUser) 
-    {
-        $AdminGroup.Add($LocalUser)
-    }
-    else 
-    {
-        Write-Warning -Message "Local user -> $UserName does not exist on $ComputerName"
-    }
+    $null = net localgroup Administrators PSRemotely /ADD
+     
+}
+
+<#
+    Below functions are needed to disable/enable the LocalAccountTokenFilterPolicy.
+    Copying the relevant section on the loopback attacks from the link here ->  https://support.microsoft.com/en-in/kb/951016
+
+    - How UAC remote restrictions work
+
+        To better protect those users who are members of the local Administrators group, we implement UAC restrictions on the network. 
+        This mechanism helps prevent against "loopback" attacks. This mechanism also helps prevent local malicious software from running remotely with administrative rights.
+        
+    - Local user accounts (Security Account Manager user account)
+
+        When a user who is a member of the local administrators group on the target remote computer establishes a remote administrative 
+        connection by using the net use * \\remotecomputer\Share$ command, for example, they will not connect as a full administrator. 
+        The user has no elevation potential on the remote computer, and the user cannot perform administrative tasks. 
+        If the user wants to administer the workstation with a Security Account Manager (SAM) account, 
+        the user must interactively log on to the computer that is to be administered with Remote Assistance or Remote Desktop, if these services are available.
+
+    - Domain user accounts (Active Directory user account)
+
+        A user who has a domain user account logs on remotely to a Windows Vista computer. 
+        And, the domain user is a member of the Administrators group. 
+        In this case, the domain user will run with a full administrator access token on the remote computer, and UAC will not be in effect. 
+
+    - UAC remote settings
+
+        The LocalAccountTokenFilterPolicy registry entry in the registry can have a value of 0 or of 1. 
+        These values change the behavior of the registry entry to the behavior that is described in the following table.
+        Value	Description
+        0	This value builds a filtered token. This is the default value. The administrator credentials are removed.
+        1	This value builds an elevated token.
+#>
+Function Disable-LocalAccountTokenFilterPolicy {
+    param()
+    Set-ItemProperty -path HkLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\  -Name LocalAccountTokenFilterPolicy -Value 1
+}
+
+Function Enable-LocalAccountTokenFilterPolicy {
+    param()
+    Set-ItemProperty -path HkLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\  -Name LocalAccountTokenFilterPolicy -Value 0
 }
