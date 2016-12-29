04e4f608d88d23312984c74957883879da4d6925
DexterPOSH <dexter34925@yahoo.in>
2016-12-29 19:57:46 +0530

    Add some of the bootstrap function tests

--- Tests/Unit/bootstrap.Tests.ps1
+++ Tests/Unit/bootstrap.Tests.ps1
@@ -22,290 +22,177 @@ InModuleScope -ModuleName $ENV:BHProjectName {
 		RequiredVersion='3.3.5';
     },
 	@{
-		ModuleName='Pester';
-		ModuleVersion='3.3.5';
+		ModuleName='PoshSpec';
+		ModuleVersion='1.1.10';
 	})
     Get-Service -Name WinRM | Restart-Service
     $Session = New-PSSession -ComputerName Localhost
     
 Describe "BootStrapRemotelyNode $PSVersion" -Tags UnitTest {
         
-    Context 'If the required module is present on the Remotely node' {
+    Context 'If the required modules are present on the Remotely node and PSRemotely Path already present' {
         # Arrange
-        Mock -CommandName TestRemotelyNodeModule -MockWith {$True} 
+        Mock -CommandName TestRemotelyNode -MockWith {@{'Pester'=$true;'PoshSpec'=$true}; $true}
+        Mock -CommandName UpdateRemotelyNodeMap -MockWith {} 
         Mock -CommandName CopyRemotelyNodeModule -MockWith {}
+        Mock -CommandName CleanupPSRemotelyNodePath -MockWith {}
         
         # Act
-        BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired 
+        BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired -PSRemotelyNodePath 'C:\temp'
         
         # Assert 
         
-        It "Should call TestRemotelyNodeModule $($ModuleRequired.Count) times" {
-            Assert-MockCalled -CommandName TestRemotelyNodeModule -Times $($ModuleRequired.Count) -Exactly  -Scope Context
+        It "Should test if the Remotely Node for the modules and PSRemotely path" {
+            Assert-MockCalled -CommandName TestRemotelyNode -Times 1 -Exactly  -Scope Context
+        }
+
+        It 'Should update the status of the Remotely Node in the beginning' {
+            Assert-MockCalled -CommandName UpdateRemotelyNodeMap -Times 1 -Exactly  -Scope Context
         }
         
-        It "Should NOT call CopyRemotelyNodeModule" {
+        It "Should archive the existing tests files on the PSRemotely path on the node" {
+            Assert-MockCalled -CommandName CleanupPSRemotelyNodePath -Times 1 -Exactly -Scope Context
+        }
+
+        It 'Should not try to copy the modules, as already present on the Remote node' {
             Assert-MockCalled -CommandName CopyRemotelyNodeModule -Times 0 -Exactly -Scope Context
         }
     }
     
-    Context 'If the required module NOT present on the Remotely node, and it is bootstrapped (copied)' {
+    Context 'If one of the required modules is NOT present on the Remotely node and PSRemotely path is present' {
         # Arrange
-        Mock -CommandName TestRemotelyNodeModule -MockWith {$False} 
-        Mock -CommandName CopyRemotelyNodeModule -MockWith {}
+        Mock -CommandName TestRemotelyNode -MockWith {@{'Pester'=$true;'PoshSpec'=$false}; $true}
+        Mock -CommandName UpdateRemotelyNodeMap -MockWith {} 
+        Mock -CommandName CopyRemotelyNodeModule -MockWith {} #-ParameterFilter {$FullyQualifiedName.ModuleName -eq 'PoshSpec'}
+        Mock -CommandName CleanupPSRemotelyNodePath -MockWith {}
         
         # Act
-        BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired 
+        BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired -PSRemotelyNodePath 'C:\temp'
         
         # Assert 
         
-        It "Should call TestRemotelyNodeModule $($ModuleRequired.Count) times" {
-            Assert-MockCalled -CommandName TestRemotelyNodeModule -Times $($ModuleRequired.Count) -Exactly -Scope Context 
+        It "Should test the Remotely Node for the modules and PSRemotely path at beginning and end" {
+            Assert-MockCalled -CommandName TestRemotelyNode -Times 2 -Exactly -Scope Context 
         }
         
-        It "Should call CopyRemotelyNodeModule $($ModuleRequired.Count) times" {
-            Assert-MockCalled -CommandName CopyRemotelyNodeModule -Times $($ModuleRequired.Count) -Exactly -Scope Context
+        It "Should only copy the missing module" {
+            Assert-MockCalled -CommandName CopyRemotelyNodeModule -Times 1 -Exactly -Scope Context
         }
+
+        It "Should archive the existing tests files on the PSRemotely path on the node" {
+            Assert-MockCalled -CommandName CleanupPSRemotelyNodePath -Times 1 -Exactly -Scope Context
+        }
+
+        It 'Should update the PSRemotely variable at the beginning and end of the bootstrap function' {
+            Assert-MockCalled -CommandName UpdateRemotelyNodeMap -Times 2 -Exactly -Scope Context
+        }
+
     }
 
-    Context 'If the required module NOT present on the Remotely node, and the bootstrapping (copy) fails' {
+    Context 'If all the required modules NOT present on the Remotely node, and PSRemotely path is present' {
         # Arrange
-        Mock -CommandName TestRemotelyNodeModule -MockWith {$False} 
-        Mock -CommandName CopyRemotelyNodeModule -MockWith {throw 'Copy Failed'}
+        Mock -CommandName TestRemotelyNode -MockWith {@{'Pester'=$false;'PoshSpec'=$false}; $true}
+        Mock -CommandName UpdateRemotelyNodeMap -MockWith {} 
+        Mock -CommandName CopyRemotelyNodeModule -MockWith {} #-ParameterFilter {$FullyQualifiedName.ModuleName -eq 'PoshSpec'}
+        Mock -CommandName CleanupPSRemotelyNodePath -MockWith {}
         
         # Act
-		$Output =   TRY {
-						 BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired  -ErrorVariable bootError
-					}
-					CATCH {
-						$Psitem.Exception.Message	
-					}
-		# Assert 
-
-        It "Should call TestRemotelyNodeModule 1 time" {
-            Assert-MockCalled -CommandName TestRemotelyNodeModule -Times 1 -Exactly -Scope Context 
+        BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired -PSRemotelyNodePath 'C:\temp'
+        
+        
+        It "Should test the Remotely Node for the modules and PSRemotely path at beginning and end" {
+            Assert-MockCalled -CommandName TestRemotelyNode -Times 2 -Exactly -Scope Context 
+        }
+        
+        It "Should copy all the missing module" {
+            Assert-MockCalled -CommandName CopyRemotelyNodeModule -Times $ModuleRequired.Count -Exactly -Scope Context
         }
 
-        It "Should call CopyRemotelyNodeModule only 1 time" { # this would throw an error so next module is not processed
-           Assert-MockCalled -CommandName CopyRemotelyNodeModule -Times 1 -Exactly -Scope Context 
+        It "Should archive the existing tests files on the PSRemotely path on the node" {
+            Assert-MockCalled -CommandName CleanupPSRemotelyNodePath -Times 1 -Exactly -Scope Context
         }
 
-		It 'Should write errors for each node the warning stream' {
-			$bootError | Should Not BeNullOrEmpty
-			$Output | Should be 'Copy Failed'
-		}
-    }
-      
+        It 'Should update the PSRemotely variable at the beginning and end of the bootstrap function' {
+            Assert-MockCalled -CommandName UpdateRemotelyNodeMap -Times 2 -Exactly -Scope Context
+        }
     }       
 
-
-Describe "TestRemotelyNodeModule $PSVersion" -Tags  UnitTest {
-    
-    Context 'If the required module NOT present on the Remotely node' {
+    Context 'If all the required modules  present on the Remotely node, and PSRemotely path is NOT present' {
         # Arrange
-        Mock -CommandName Invoke-Command -MockWith {} -ParameterFilter { $ScriptBlock.ToString().Equals('Get-Module -ListAvailable -Name $Using:FullyQualifiedName.Name')} -Verifiable
+        Mock -CommandName TestRemotelyNode -MockWith {@{'Pester'=$true;'PoshSpec'=$true}; $false}
+        Mock -CommandName UpdateRemotelyNodeMap -MockWith {} 
+        Mock -CommandName CopyRemotelyNodeModule -MockWith {} #-ParameterFilter {$FullyQualifiedName.ModuleName -eq 'PoshSpec'}
+        Mock -CommandName CleanupPSRemotelyNodePath -MockWith {}
+        Mock -CommandName CreatePSRemotelyNodePath -MockWith {}
         
         # Act
-        $Output = TestRemotelyNodeModule -Session $session -FullyQualifiedName $ModuleRequired[0]
+        BootStrapRemotelyNode -Session $Session -FullyQualifiedName $ModuleRequired -PSRemotelyNodePath 'C:\temp'
+        
+        
+        It "Should test the Remotely Node for the modules and PSRemotely path at beginning and end" {
+            Assert-MockCalled -CommandName TestRemotelyNode -Times 2 -Exactly -Scope Context 
+        }
+
+        It 'Should create the PSRemotelyNodePath on the remote node' {
+            Assert-MockCalled -CommandName CreatePSRemotelyNodePath -Times 1 -Exactly -Scope Context
+        }
+        
+        It "Should NOT copy modules , already present" {
+            Assert-MockCalled -CommandName CopyRemotelyNodeModule -Times 0 -Exactly -Scope Context
+        }
+
+        It "Should NOT archive the existing tests files on the PSRemotely path on the node, since the folder is not present" {
+            Assert-MockCalled -CommandName CleanupPSRemotelyNodePath -Times 0 -Exactly -Scope Context
+        }
+
+        It 'Should update the PSRemotely variable at the beginning and end of the bootstrap function' {
+            Assert-MockCalled -CommandName UpdateRemotelyNodeMap -Times 2 -Exactly -Scope Context
+        }
+    }       
+
+}
+
+Describe "TestRemotelyNode $PSVersion" -Tags  UnitTest {
+    
+    Context 'check if the required arguments are passed to the Invoke-Command' {
+        # Arrange
+        Mock -CommandName Invoke-Command -MockWith {} 
+        
+        # Act
+        $Output = TestRemotelyNode -Session $session -FullyQualifiedName $ModuleRequired -PSRemotelyNodePath 'C:\temp'
         
         # Assert
         It 'Should call Invoke-Command to fetch if the module is installed' {
-            Assert-MockCalled -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Equals('Get-Module -ListAvailable -Name $Using:FullyQualifiedName.Name')} -Times 1 -Exactly 
-			Assert-VerifiableMocks
+            Assert-MockCalled -CommandName Invoke-Command -ParameterFilter { 
+                ($ArgumentList -contains 'C:\temp') -and
+                ($ArgumentList.Count -eq 2) -and
+                ($Session -ne $null)
+            } -Times 1 -Exactly 
         }
-        
-        It 'Should return False' {
-            $Output | Should Be $False
-        }
+
     }
-    
-    Context 'If the required module is present on the Remotely node' {
-        # Arrange
-        Mock -CommandName Invoke-Command -MockWith {'DummyValue'}
-        
-        # Act
-        $Output = TestRemotelyNodeModule -Session $session -FullyQualifiedName $ModuleRequired[0]
-        
-        # Assert
-        
-        It 'Should return True' {
-            $Output | Should Be $True
-        }
-    }  
-    
 }
 
-Describe "CreateRemotelyNodePath $PSVersion" -Tags UnitTest {
+Describe "CreatePSRemotelyNodePath $PSVersion" -Tags UnitTest {
 
-	Context "If the Path exists on the Remotely Node" {
+	Context "Check if the correct path argument gets passed to the Invoke-Command" {
 		# Arrange
-		Mock -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Equals('Test-Path -Path $using:Path')} -MockWith {$True} -Verifiable
-		Mock -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Contains('New-Item')}  -MockWith {}
+		Mock -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Equals('$null = New-Item -Path "$using:Path\lib\Artifacts" -ItemType Directory -Force')}
 
 		# Act
-		CreateRemotelyNodePath -Session $session -path 'C:\temp'
+		CreatePSRemotelyNodePath -Session $session -path 'C:\temp'
 
 		# Assert
-		It 'Should call Invoke-Command to check if the path on RemotelyNode exists' {
-			Assert-MockCalled -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Equals('Test-Path -Path $using:Path')} -Times 1 -Exactly
-			Assert-VerifiableMocks
+		It 'Should call Invoke-Command to create the path on RemotelyNode' {
+			Assert-MockCalled -CommandName Invoke-Command -Times 1 -Exactly -Scope Context
 		}
 
-		It 'Should not create the path as it already exists' {
-			Assert-MockCalled -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Contains('New-Item')}  -Times 0 -Exactly
-		}
-	}
-
-	Context "If the Path does NOT exist on the Remotely Node, it gets created" {
-		# Arrange
-		Mock -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Equals('Test-Path -Path $using:Path')} -MockWith {$false} -Verifiable
-		Mock -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Contains('New-Item')} -MockWith {} -Verifiable
-		
-		# Act
-		CreateRemotelyNodePath -Session $session -path 'C:\temp'
-
-		# Assert
-		It 'Should  create the path.' {
-			Assert-MockCalled -CommandName Invoke-Command -ParameterFilter { $ScriptBlock.ToString().Contains('New-Item')} -Times 1 -Exactly
-			Assert-VerifiableMocks
-		}
 	}
 }
 
 Describe "CopyRemotelyNodeModule $PSversion" -Tag UnitTest {
 	
-	Context "Required Module present locally, Copied successfully to the Remotely Node." {
-		# Arrange
-		Mock -CommandName Get-Module -ParameterFilter {$ListAvailable -and ($Name -eq 'Pester')} -MockWith {[pscustomobject]@{Path='C:\temp\Pester.psd1'}}
-		Mock -CommandName Split-Path -ParameterFilter {$Path -eq 'C:\temp\Pester.psd1'}
-		Mock -CommandName CreateRemotelyNodePath -MockWith {}
-		Mock -CommandName CopyModuleFolderToRemotelyNode -MockWith {}
-		# Act
-		$OutPut = 	CopyRemotelyNodeModule -Session $Session -FullyQualifiedName $ModuleRequired[0]
 
-		# Assert
-		It 'Should not return anything when it succeeds' {
-			$Output | Should BeNullOrEmpty
-		}
-
-		It 'Should call get-Module to verify that module is installed locally' {
-			Assert-MockCalled -CommandName Get-Module -ParameterFilter {$ListAvailable -and ($Name -eq 'Pester')} -Times 1 -Exactly -Scope Context
-		}
-
-		It 'Should call Split-Path to get the parent path to the Module' {
-			Assert-MockCalled -CommandName Split-Path -ParameterFilter {$Path -eq 'C:\temp\Pester.psd1'} -Times 1 -Exactly -Scope Context
-		}
-
-		It 'Should create the Folder for the module on the RemotelyNode' {
-			Assert-MockCalled -CommandName CreateRemotelyNodePath -Times 1 -Exactly -Scope Context
-		}
-
-		It 'Should copy the local module folder to the remotely node' {
-			Assert-MockCalled -CommandName CopyModuleFolderToRemotelyNode -Times 1 -Exactly -Scope Context
-		}
-	}
-
-	Context "Required Module present locally, Copy failed to the Remotely Node." {
-		# Arrange
-		Mock -CommandName Get-Module -ParameterFilter {$ListAvailable -and ($Name -eq 'Pester')} -MockWith {[pscustomobject]@{Path='C:\temp\Pester.psd1'}}
-		Mock -CommandName Split-Path -ParameterFilter {$Path.Equals('C:\temp\Pester.psd1')}
-		Mock -CommandName CreateRemotelyNodePath -MockWith {}
-		Mock -CommandName CopyModuleFolderToRemotelyNode -MockWith {throw 'Copy Failed'}
-		# Act
-		$OutPut =   TRY {
-						CopyRemotelyNodeModule -Session $Session -FullyQualifiedName $ModuleRequired[0] -ErrorVariable copyError -ErrorAction Stop
-					}
-					CATCH {
-
-					}
-
-		# Assert
-		It 'Should not return anything when it succeeds' {
-			$Output | Should BeNullOrEmpty
-		}
-
-		It 'Should try to copy the local module folder to the remotely node' {
-			Assert-MockCalled -CommandName CopyModuleFolderToRemotelyNode -Times 1 -Exactly -Scope Context
-		}
-
-		It 'Should throw an exception, if copy fails' {
-			$copyError | Should Not BeNullOrEmpty
-			# TO DO add test for the exception message
-		}
-	}
-
-	Context "Required Module present locally, creating path failed on the Remotely Node." {
-		# Arrange
-		Mock -CommandName Get-Module -ParameterFilter {$ListAvailable -and ($Name -eq 'Pester')} -MockWith {[pscustomobject]@{Path='C:\temp\Pester.psd1'}}
-		Mock -CommandName Split-Path -ParameterFilter {$Path.Equals('C:\temp\Pester.psd1')}
-		Mock -CommandName CreateRemotelyNodePath -MockWith {throw 'Path creation failed'}
-		Mock -CommandName CopyModuleFolderToRemotelyNode -MockWith {throw 'Copy Failed'}
-		# Act
-		$Output =   TRY {
-						CopyRemotelyNodeModule -Session $Session -FullyQualifiedName $ModuleRequired[0] -ErrorVariable copyError -ErrorAction Stop
-					}
-					CATCH {
-						$PSItem.Exception.Message
-					}
-
-		# Assert
-		
-		It 'Should try to create the module path on the remotely node' {
-			Assert-MockCalled -CommandName CreateRemotelyNodePath -Times 1 -Exactly -Scope Context
-		}
-
-		It 'Should never reach the step to  copy the local module folder to the remotely node' {
-			Assert-MockCalled -CommandName CopyModuleFolderToRemotelyNode -Times 0 -Exactly -Scope Context
-		}
-
-		It 'Should throw an exception, if path creation fails' {
-			$copyError | Should Not BeNullOrEmpty
-			$Output | Should be 'Path Creation Failed'
-		}
-	}
-
-	Context "Required Module is not present locally, install it from PSGallery locally and then copy to remotely node" {
-		# Arrange
-		$Script:GetModuleCounter = 1
-		Mock -CommandName Get-Module -MockWith {
-			if ($Script:GetModuleCounter -eq 1) {
-				$Script:GetModuleCounter++ 
-				$null # return nothing first time
-			}
-			else{
-				[pscustomobject]@{Path='C:\temp\Pester.psd1'}
-			}
-		} 
-		Mock -CommandName Write-Warning -MockWith {}
-		Mock -Commandname Import-Module -MockWith {} -ParameterFilter {$Name -eq 'PackageManagement'}
-		Function Install-Module {param($name,$RequiredVersion)} # dummy install-module 
-		Mock -CommandName Install-Module -ParameterFilter {$name -eq 'pester' -and $RequiredVersion -eq '3.3.5'} -MockWith {}
-		Mock -CommandName Split-Path -ParameterFilter {}
-		Mock -CommandName CreateRemotelyNodePath -MockWith {}
-		Mock -CommandName CopyModuleFolderToRemotelyNode -MockWith {}
-
-		# Act
-		$Output =  CopyRemotelyNodeModule -Session $Session -FullyQualifiedName $ModuleRequired[0]
-
-		# Assert
-		It 'Should not find the module at first and throw a warning ' {
-			Assert-MockCalled -CommandName Write-Warning -Scope Context -times 1 -Exactly
-		}
-		
-		It 'Should call the Get-Module 2 times' {
-			$Script:GetModuleCounter | Should be 2
-			Assert-MockCalled -CommandName Get-Module -Times 2 -Exactly -Scope Context
-		}
-
-		It 'Should then import PackageManagement module to fetch the module' {
-			Assert-MockCalled -CommandName Import-Module -ParameterFilter {$Name -eq 'PackageManagement'} -Scope Context -times 1 -Exactly
-		}
-
-		It 'Should call Install-Module to install the required version of the module'  {
-			Assert-MockCalled -CommandName Install-Module -ParameterFilter {$name -eq 'pester' -and $RequiredVersion -eq '3.3.5'} -Times 1 -Exactly -Scope Context
-		}
-
-	}
 }
 
 }     
