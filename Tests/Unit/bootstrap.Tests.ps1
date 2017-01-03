e3af76fc5c5a254aa29775d24a70ebb70406024b
DexterPOSH <dexter34925@yahoo.in>
2017-01-03 11:07:42 +0530

    Remove the PSSession creation

--- Tests/Unit/bootstrap.Tests.ps1
+++ Tests/Unit/bootstrap.Tests.ps1
@@ -7,7 +7,6 @@ if(-not $ENV:BHProjectPath)
 $PSVersion = $PSVersionTable.PSVersion.Major
 Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
 Import-Module (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -Force
-$Session = New-PSSession -ComputerName Localhost
 
 InModuleScope -ModuleName $ENV:BHProjectName {
     # Module Preamble - put initialization code here
